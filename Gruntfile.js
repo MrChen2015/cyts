7145dfb549524692f32dc050373a250ae533e10d
MrChen2015 <562638230@qq.com>
2016-06-01 18:44:17 +0800

    bbb
    ssss

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,6 @@ module.exports = function(grunt){
 			stripBanners:true,
 			banner:'/* <%=pkg.name%>-<%=pkg.version%> <%= grunt.template.today("yyyy-mm-dd") %> */\n/* description : <%= pkg.description%> */\n/* author : <%=pkg.author%> */\n/* license : <%=pkg.license%> */\n'
 		},
-		// build:{
-		// 	src:"src/js/base.js",
-		// 	dest:"dist/base-<%=pkg.version%>.min.js"
-		// }
 		target:{
 			files: {
         	'dist/js/output-<%=pkg.version%>.min.js': ['src/js/*.js']
@@ -41,13 +37,90 @@ module.exports = function(grunt){
 			}
 		},
 		watch:{
+			livereload: {
+        	options: {
+        	  livereload: '<%=connect.server.options.livereload%>'  //监听前面声明的端口  35729
+        	},
+        	files:["Gruntfile.js","src/js/*.js","src/css/*.css","src/less/*.less","src/coffee/*.coffee","src/images/*.{png,jpg}","src/*.html"]
+        },
 			build:{
-				files:["Gruntfile.js","src/js/*.js","src/css/*.css"],
-				tasks:["jshint","uglify"],
+				files:["Gruntfile.js","src/js/*.js","src/css/*.css","src/less/*.less","src/coffee/*.coffee","src/images/*.{png,jpg}","src/*.html"],
+				tasks:["coffee","less","jshint","uglify"],
 				options:{spawn:false}
-			}
-		}
+			},
+			//run unit tests with karma (server needs to be already running) 
+  			karma: {
+  			  files: ['dist/js/*.js', 'test/*.js'],
+  			  tasks: ['karma:unit:run'] //NOTE the :run flag 
+  			}
 
+		},
+		clean:{
+			build:["dist/js/*.js","dist/css/*.css"],
+		},
+		copy:{
+			main:{
+				expand:true,
+				cwd:"dist",
+				src:"**",
+				dest:"old/<%= grunt.template.today('yyyy-mm-dd hh：MM：ss')%>/"
+			}
+		},
+		karma:{
+			options:{
+				files:["test/*.js"]
+			},
+			unit:{
+				files:[
+					{ 
+						src:["test/*.js"],
+						port: 9999,
+    					browsers: ['Chrome', 'Firefox']
+					}
+				],
+				background: true,
+    			singleRun: false
+			}
+		},
+		coffee: {
+  			compileWithMapsDir: {
+  			  options: {
+  			    sourceMap: true,
+  			    sourceMapDir: 'src/js/maps/' // source map files will be created here 
+  			  },
+  			  files: [{
+                    expand: true,
+                    cwd: 'src/coffee',
+                    src: ['**/*.coffee'],
+                    dest: 'src/js',
+                    ext: '.js'
+                }]
+  			},
+		},
+		 less: {
+            development: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/less',
+                    src: ['**/*.less'],
+                    dest: 'src/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        connect: {
+    		server: {
+    		  options: {
+    		    port: 9001,
+    		    hostname:"*",
+    		    keepalive: true,
+    		    open:true,
+    		    base: 'src'
+    		    livereload: 35729  //声明给 watch 监听的端口
+    		  }
+   		 	}
+  		}
+  		 
 	});
 	
 	grunt.loadNpmTasks("grunt-contrib-uglify");
@@ -55,6 +128,12 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-csslint");
 	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.registerTask("default",["jshint","csslint","uglify","cssmin","watch"]);
-	grunt.log.writeln('Hello, world.');
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-karma");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-connect");
+	grunt.registerTask("default",["coffee","less","jshint","csslint",/*"copy",手动复制吧 grunt copy命令*/"clean","uglify","cssmin","connect:server","watch"]);
+	// grunt.log.writeln('Hello, world.');
 };
---
+++ src/js/coffee1.js
@@ -0,0 +1,14 @@
+(function() {
+  var cube, square;
+
+  square = function(x) {
+    return x * x;
+  };
+
+  cube = function(x) {
+    return square(x) * x + 1;
+  };
+
+}).call(this);
+
+//# sourceMappingURL=maps/coffee1.js.map
